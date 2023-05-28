c3be515cc011ab7737b3c3e87a5b9ac6528ba5e7
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2023-05-28 15:40:20 +0100

    feat(fetchMessages): Conversation History API
    Learning to use https://api.slack.com/methods/conversations.history
    Get messages, thread parents and reactions to messages since a certain
    point in time.

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const app = new App({
     port: process.env.PORT || 3000
 });
 
+// Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
+const { WebClient, LogLevel } = require("@slack/web-api");
+
+// WebClient instantiates a client that can call API methods
+// When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+const webClient = new WebClient(process.env.SLACK_BOT_TOKEN, {
+  // LogLevel can be imported and used to make debugging simpler
+  logLevel: LogLevel.DEBUG
+});
+
 // Find conversation ID using the conversations.list method
 async function findConversation(name) {
     try {
@@ -60,6 +70,31 @@ async function postMessage(channelId, message){
   }  
 }
 
+// read more on retrieving conversation history 
+// https://api.slack.com/methods/conversations.history
+async function fetchMessages(channelId, uptoTimeInEpoch, inclusive, limitTo){
+    //fetch all messages from channel in the last 24 hours
+    // WebClient instantiates a client that can call API methods
+    // When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+  try {
+        // Call the conversations.history method using the built-in WebClient
+        const result = await app.client.conversations.history({
+            // The token you used to initialize your app
+            token: process.env.SLACK_BOT_TOKEN,
+            channel: channelId,
+            // In a more realistic app, you may store ts data in a db
+            latest: uptoTimeInEpoch,
+            // Limit results
+            inclusive: inclusive,
+            limit: limitTo
+          });
+          return result.messages || [];
+  }
+  catch (error) {
+    console.error(error);
+  }  
+}
+
 
 // Your app can listen to events if you enable socket mode!
 // https://slack.dev/bolt-js/tutorial/getting-started#setting-up-events
@@ -93,11 +128,34 @@ app.action("button_click", async({body, ack, say}) => {
     await say(`<@${body.user.id}> clicked the button! Good stuff! Thanks for responding!`);
 });
 
+
+function getYesterday(){
+    let today = new Date();
+    let yesterday = new Date();
+    yesterday.setDate(today.getDate() - 1);
+    return yesterday;
+}
+
+function getTimeInPast(timeSinceInMilliSeconds){
+    let today = new Date();
+    let yesterday = new Date();
+    yesterday.setTime(today.getTime() - timeSinceInMilliSeconds);
+    return yesterday;
+}
+
 (async() => {
     // start app
     await app.start();
 
     console.log('⚡️ Bolt app is running!');
     var channelId = await findConversation("learn-slack-api");
-    await postMessage(channelId, "hello, I'm testing slack api");
+    //await postMessage(channelId, "hello, I'm testing slack api");
+    // Fetch message using a channel ID and message TS
+    
+    let yesterday = getTimeInPast(60*60*1000);
+    let messagesSince = await fetchMessages(channelId, yesterday.getTime(), true, 100);
+    console.log("retrieved " + messagesSince.length +  " messages: ");
+    messagesSince.forEach(
+            element => console.log(element)
+          );
 })();
